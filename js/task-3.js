11de3d2fabd426ad7b0068cd2936ccf86e953ede
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-03-25 14:51:07 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
+
 function checkForSpam(message) {
-  const normalizedMessage = message.toLowerCase();
-  return normalizedMessage.includes('spam') || normalizedMessage.includes('sale');
+    return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
 }
+
+
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
